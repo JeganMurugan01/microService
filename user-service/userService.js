e07adf5ffd32100168b50b33cb8d5cbef180c8c4
Jegan Murugan <jegan.murugan@concertidc.com>
2024-06-24 13:50:14 +0530

    getUsers

--- user-service/userService.js
+++ user-service/userService.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const userDate = require('../api-getway/userdata');
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send({ data: 'User server is online' });
+  res.status(200).send({ data: userDate });
 });
 
 app.listen(port, () => {
